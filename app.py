f0610bc6e36bb83902904873ce83de085b7bd978
Adesanya Olamide <105758317+sha-doow@users.noreply.github.com>
2024-06-25 23:57:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.title("Hate Speech Detection")
 user_input = st.text_area("Enter a Tweet:")
 
 if user_input:
-    st.chat_input(prediction = hate_speech_detection(user_input))
-    st.write(f"Prediction: {prediction}")
+    message = st.chat_message(prediction = hate_speech_detection(user_input))
+    st.chat_input(f"Prediction: {prediction}")
     
     
