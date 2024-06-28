65cb686eddb06b813254c9ceb47f6f565ced6a3c
Adesanya Olamide <105758317+sha-doow@users.noreply.github.com>
2024-06-28 11:46:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,31 +11,8 @@ def hate_speech_detection(tweet):
     return prediction[0]
 
 st.title("Hate Speech Detection")
-prompt = st.chat_input("Enter Your Tweet")
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+user_input = st.text_area("Enter a Tweet:")
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
- # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-response = f"Echo: {prompt}"
-# Display assistant response in chat message container
-with st.chat_message("assistant"):
-    st.markdown(response)
-# Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": response})
-
-if prompt:
-    prediction = hate_speech_detection(prompt)
+if user_input:
+    prediction = hate_speech_detection(user_input)
     st.write(f"Prediction: {prediction}")
-    
-    
